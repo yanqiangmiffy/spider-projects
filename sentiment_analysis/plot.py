123ac462d3103203398408df5395814a9a69318d
yanqiangmiffy <1185918903@qq.com>
2019-05-18 20:43:54 +0800

    更新

--- sentiment_analysis/plot.py
+++ sentiment_analysis/plot.py
@@ -13,11 +13,11 @@ from pylab import mpl
 
 mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
-import matplotlib
-
-a = sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
-for i in a:
-    print(i)
+# import matplotlib
+#
+# a = sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
+# for i in a:
+#     print(i)
 
 df1 = pd.read_excel('plotdata.xlsx', sheet_name=0)
 df2 = pd.read_excel('plotdata.xlsx', sheet_name=1)
@@ -31,7 +31,7 @@ for df in [df1, df2, df3]:
     k_range = [i for i in range(1, 1 + len(df))]
     train_scores = df['频数'].values
     plt.plot(k_range, train_scores, label=df.columns[0])
-plt.xlabel('数据')
-plt.ylabel('频数')
-plt.legend()  # show legned
-plt.show()
+    plt.xlabel('数据')
+    plt.ylabel('频数')
+    plt.legend()  # show legned
+    plt.show()
